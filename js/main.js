ff8b71e9cd4ab55f58ae623fcdab27f261610e4d
EduardsZ <eduards.zvaigzne@gmail.com>
2022-01-27 20:20:55 +0000

    some changes in js file

---
+++ js/main.js
@@ -0,0 +1,11 @@
+console.log('hello')
+
+/**
+ * just summs two variables
+ * @param {*} a represents irst argument
+ * @param {*} b represents second argument
+ * @returns any
+ */
+const sum = (a, b) => a + b
+
+console.log(sum(5, 7))
